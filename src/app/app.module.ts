4af8f71a9d3c07c22fac0257cf525bfeb03055f6
vigneshkumar143 <vigneshkumar9194@gmail.com>
2023-01-04 22:52:35 +0530

    Initial single page application setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    RouterModule.forRoot([
+      {path: 'navbar', component: NavbarComponent},
+      {path: 'login', component: LoginComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
