cd9aae5338b509ee23416d63aecf7f7cf20020db
vigneshkumar143 <vigneshkumar9194@gmail.com>
2023-01-07 22:22:57 +0530

    The 25% work completed for landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -9,26 +9,30 @@ import { LandingPageStructure } from './myLanding.model'
 export class LandingComponent {
 	public landigpageList: LandingPageStructure[] = [{
 		  id: 'art-123',
-		  category: 'category 1',
-		  title: 'Title 1',
+		  category: 'relationships',
+		  title: '10 Readers Share Their Happy Moments',
+      description: 'From impromptu weddings to car picnics.',
       backgroundImage: 'bgImg.jpg'
     },
     {	
       id: 'art-456',
-		  category: 'catetory 2',
-		  title: 'Title 2',
+		  category: 'designs',
+		  title: 'A Living Room Fakeover',
+      description: 'We recently swapped two things in our apartment...',
     	backgroundImage: 'bgImg1.jpg'
     },
     { 
       id: 'art-456',
-      category: 'catetory 3',
-      title: 'Title 3',
+      category: 'relationships',
+      title: 'How to Say Goodbye at a Party',
+      description: 'I will die on this hill.',
       backgroundImage: 'bgImg2.jpg'
     },
     { 
       id: 'art-456',
-      category: 'catetory 4',
-      title: 'Title 4',
+      category: 'relationships',
+      title: 'Race matters: Traveling While Black',
+      description: 'I can’t take for granted that I’ll be welcomed with open arms. One memory comes to mind...',
       backgroundImage: 'bgImg3.jpg'
     }];
 }
--- src/app/landing/myLanding.model.ts
+++ src/app/landing/myLanding.model.ts
@@ -3,5 +3,6 @@ export interface LandingPageStructure
 	id: string;
 	category: string;
 	title: string;
+	description: string;
   	backgroundImage: string;
 }
