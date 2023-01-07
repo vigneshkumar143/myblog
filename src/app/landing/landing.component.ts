66bc4d7480647b890109dad55af50a213a679171
vigneshkumar143 <vigneshkumar9194@gmail.com>
2023-01-07 21:47:02 +0530

    Landing page and images are added

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LandingPageStructure } from './myLanding.model'
 
 @Component({
   selector: 'app-landing',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-
+	public landigpageList: LandingPageStructure[] = [{
+		  id: 'art-123',
+		  category: 'category 1',
+		  title: 'Title 1',
+      backgroundImage: 'bgImg.jpg'
+    },
+    {	
+      id: 'art-456',
+		  category: 'catetory 2',
+		  title: 'Title 2',
+    	backgroundImage: 'bgImg1.jpg'
+    },
+    { 
+      id: 'art-456',
+      category: 'catetory 3',
+      title: 'Title 3',
+      backgroundImage: 'bgImg2.jpg'
+    },
+    { 
+      id: 'art-456',
+      category: 'catetory 4',
+      title: 'Title 4',
+      backgroundImage: 'bgImg3.jpg'
+    }];
 }
---
+++ src/app/landing/myLanding.model.ts
@@ -0,0 +1,7 @@
+export interface LandingPageStructure
+{
+	id: string;
+	category: string;
+	title: string;
+  	backgroundImage: string;
+}
